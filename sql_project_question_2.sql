22c72bdbd69ca88eaf4f0f6df4cdfa1073e6df6b
Tereza Cetkovska <tera.seb@seznam.cz>
2023-10-29 19:58:01 +0100

    SQL for question 2.

---
+++ sql_project_question_2.sql
@@ -0,0 +1,16 @@
+SELECT 
+	ttc.payroll_year, 
+	round(avg(ttc.value_payroll),0) AS average_wage,
+	ttc.category_code,
+	cpc.name, 
+	round(ttc.average_value_price,0) AS average_price,
+	round(avg(ttc.value_payroll)/ttc.average_value_price,0) AS how_many_litres_kilos,
+	cpc.price_unit 
+FROM t_tereza_cetkovska_project_SQL_primary_final ttc
+LEFT JOIN czechia_price_category cpc 
+	ON cpc.code = ttc.category_code 
+WHERE ttc.value_type_code = 5958
+	AND ttc.calculation_code = 200
+	AND ttc.category_code IN (114201,111301)
+	AND ttc.payroll_year IN (2006,2018)
+GROUP BY ttc.payroll_year, ttc.category_code; 
