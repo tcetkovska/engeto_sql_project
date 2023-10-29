fb759957726986646ccb7066a17882f5044a9a27
Tereza Cetkovska <tera.seb@seznam.cz>
2023-10-29 20:44:34 +0100

    SQL for question 3.

--- sql_project_question_3.sql
+++ sql_project_question_3.sql
@@ -1,16 +1,29 @@
-SELECT 
-	ttc.payroll_year, 
-	round(avg(ttc.value_payroll),0) AS average_wage,
+
+CREATE OR REPLACE TABLE t_tereza_cetkovska_auxiliary AS
+SELECT
+	ttc.year_price,
 	ttc.category_code,
-	cpc.name, 
+	cpc.name,
 	round(ttc.average_value_price,0) AS average_price,
-	round(avg(ttc.value_payroll)/ttc.average_value_price,0) AS how_many_litres_kilos,
 	cpc.price_unit 
 FROM t_tereza_cetkovska_project_SQL_primary_final ttc
 LEFT JOIN czechia_price_category cpc 
 	ON cpc.code = ttc.category_code 
-WHERE ttc.value_type_code = 5958
-	AND ttc.calculation_code = 200
-	AND ttc.category_code IN (114201,111301)
-	AND ttc.payroll_year IN (2006,2018)
-GROUP BY ttc.payroll_year, ttc.category_code; 
+	WHERE ttc.year_price IS NOT NULL
+GROUP BY ttc.year_price, ttc.category_code;  
+
+SELECT 
+	ttca.category_code,
+	ttca.name,
+	ttca.year_price,
+	ttca.average_price, 
+	ttca2.year_price AS previous_year,
+	ttca2.average_price AS previous_year_price,
+	round(ttca.average_price/ttca2.average_price-1,2) AS year_on_year_percentage_change
+FROM t_tereza_cetkovska_auxiliary ttca 
+LEFT JOIN t_tereza_cetkovska_auxiliary ttca2
+	ON ttca2.year_price + 1 = ttca.year_price 
+	AND ttca2.category_code = ttca.category_code 
+	WHERE ttca2.year_price IS NOT NULL 
+GROUP BY ttca.category_code, ttca.year_price  
+
