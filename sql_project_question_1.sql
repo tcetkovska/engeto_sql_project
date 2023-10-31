54f75fb4e9766c5832e4b0214e5a69869bb0d0f4
Tereza Cetkovska <tera.seb@seznam.cz>
2023-10-31 19:55:08 +0100

    SQL for question 1.

--- sql_project_question_1.sql
+++ sql_project_question_1.sql
@@ -36,14 +36,14 @@ LEFT JOIN (
    				round(avg(tc.value_payroll),0) AS average_wage
    			FROM t_tereza_cetkovska_project_SQL_primary_final tc
    			WHERE tc.industry_branch_code IS NOT NULL
- 	AND tc.value_type_code = 5958
-	AND tc.calculation_code = 200
+ 				AND tc.value_type_code = 5958
+				AND tc.calculation_code = 200
    			GROUP BY tc.payroll_year, tc.industry_branch_code
    		    ) tc2
    	ON tc2.payroll_year+1=tc.payroll_year
    	AND tc2.industry_branch_code=tc.industry_branch_code
  LEFT JOIN czechia_payroll_industry_branch cpib
- 	ON	cpib.code = tc.industry_branch_code
+ 	ON cpib.code = tc.industry_branch_code
  WHERE tc.industry_branch_code IS NOT NULL
  	AND tc2.payroll_year IS NOT NULL
  	AND tc.value_type_code = 5958
